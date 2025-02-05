f6c58f00978a864e1e6f2e53a685073c858185db
Anchal Agrawal <anchalagrawal2411@gmail.com>
2025-02-05 23:09:22 +0530

    trying for react routers

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import "./styles.css";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/RecipeApp">
   <StrictMode>
     <App />
   </StrictMode>
